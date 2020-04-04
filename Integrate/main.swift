2b0eb73af4f8a203ba0f6feeb7df7faf3a6bc454
ronaldogomes96 <gomes.ronaldo96@gmail.com>
2020-04-04 14:48:01 -0300

    Criada uma funcao para o menu incial

--- Integrate/main.swift
+++ Integrate/main.swift
@@ -9,6 +9,13 @@
 import Foundation
 import Accelerate
 
+//Variaveis Globais
+var intervalos: [String] = []
+var range: ClosedRange<Double> = 0 ... 0
+
+// variavel que recebe a entrada do usuario para uso da NSExpression
+var formula: String = ""
+
 // enum para representar se o dado e funcao ou intervalo
 enum TypeInput{
     case function
@@ -44,84 +51,113 @@ func mostraOpcoes(type: TypeInput, input: String){
         """)
 }
 
-var intervalos: [String] = []
-var range: ClosedRange<Double> = 0 ... 0
-
-// variavel que recebe a entrada do usuario para uso da NSExpression
-var formula: String = ""
-
-print("Calculo de integrais: 📚🧐 \n")
-
-print("""
-Esse script resolve integrais definidas.
-Digite + para adição
-Digite - para subtração
-Digite * para multiplicação
-Digite / para divisão
-Digite x para representar a variavel da função
-Digite ( ) para inserir parenteses
-Exemplo de um modelo de função: x + 1 / ( 2*x - 5 )
-
-""")
-
-print("""
-Entre com a sentença matemática:
-""")
-
-// Utiliza o readline para pegar a equação digitada pelo usúario.
-while let input = readLine() {
-    guard input.lowercased() != "sim", input.lowercased() != "s" else {
-        print("-------------------------------------------------------\n")
-        break
-    }
-
-    if input.count != 0 {
-        mostraOpcoes(type: TypeInput.function, input: input)
-        formula = input
-    } else {
-        print("Dado invalido, digite novamente:")
-        continue
-    }
-}
-
-// Utiliza o readline para pegar os intervalos digitados pelo usúario.
-print("""
-Agora, digite os intervalos:
-ex: 1, 2
-""")
-while let input = readLine() {
-    guard input.lowercased() != "sim", input.lowercased() != "s" else {
-        print("-------------------------------------------------------\n")
-        break
-    }
-
-    intervalos = input.components(separatedBy: ", ")
-    if intervalos.count == 2 {
-        range = Double(intervalos[0])! ... Double(intervalos[1])!
-        mostraOpcoes(type: TypeInput.interval, input: input)
-    }
-}
-
-// Configurações padrão do Quadrature sobre intervalos máximo e tolerância de erros.
-let quadrature = Quadrature(integrator:.qags(maxIntervals: 10000),
-                            absoluteTolerance: 1.0e-8,
-                            relativeTolerance: 1.0e-2)
-
-// Onde calcula o integral com o Quadrature.
-let result = quadrature.integrate(over: range) { x in
-    // aqui eh montado o dicinario que a NSExpression necessita
-    let intDictionary = ["x": x]
+func inputUsuario(tipodeInput: TypeInput){
     
-    // aqui onde eh calculado o valor da equacao baseado na string formula
-    if let timesResult = formula.expression.expressionValue(with: intDictionary, context: nil) as? Double {
-        return timesResult
-    } else { return 0 } // caso de algum erro, deve-se ainda fazer uma verificacao mais fiel, por o return 0 causara erros na saida
+    while let input = readLine() {
+        
+        guard input.lowercased() != "sim", input.lowercased() != "s" else {
+            print("-------------------------------------------------------\n")
+            break
+        }
+        
+        //Verifica se é uma funcao ou um intervalo
+        
+        if tipodeInput == TypeInput.function {
+            
+            // Utiliza o readline para pegar a equação digitada pelo usúario.
+            if input.count != 0 {
+                mostraOpcoes(type: TypeInput.function, input: input)
+                formula = input
+            } else {
+                print("Dado invalido, digite novamente:")
+                continue
+            }
+        }
+        
+        else if tipodeInput == TypeInput.interval {
+            
+            // Utiliza o readline para pegar os intervalos digitados pelo usúario.
+            intervalos = input.components(separatedBy: ", ")
+            if intervalos.count == 2 {
+                range = Double(intervalos[0])! ... Double(intervalos[1])!
+                mostraOpcoes(type: TypeInput.interval, input: input)
+            }
+        }
+    }
 }
 
-// Mostra o resultado (sucesso ou erro) do cálculo da integral.
-switch result {
-    case .success(let integralResult):
-        print("Integral calculada:\n\(integralResult)")
-    case .failure(let error):
-        print("Erro no cálculo da integral:", error.errorDescription)
+func Menu() {
+    
+    print("Calculo de integrais: 📚🧐 \n")
+
+    print("""
+    Esse script resolve integrais definidas.
+    Digite + para adição
+    Digite - para subtração
+    Digite * para multiplicação
+    Digite / para divisão
+    Digite x para representar a variavel da função
+    Digite ( ) para inserir parenteses
+    Exemplo de um modelo de função: x + 1 / ( 2*x - 5 )
+
+    """)
+
+    print("""
+    Entre com a sentença matemática:
+    """)
+
+    inputUsuario(tipodeInput: TypeInput.function)
+
+    // Utiliza o readline para pegar os intervalos digitados pelo usúario.
+    print("""
+    Agora, digite os intervalos:
+    ex: 1, 2
+    """)
+    
+    inputUsuario(tipodeInput: TypeInput.interval)
 }
+
+while(true){
+    // Iniciando programa inicial
+    Menu()
+
+    // Configurações padrão do Quadrature sobre intervalos máximo e tolerância de erros.
+    let quadrature = Quadrature(integrator:.qags(maxIntervals: 10000),
+                                absoluteTolerance: 1.0e-8,
+                                relativeTolerance: 1.0e-2)
+
+    // Onde calcula o integral com o Quadrature.
+    let result = quadrature.integrate(over: range) { x in
+        // aqui eh montado o dicinario que a NSExpression necessita
+        let intDictionary = ["x": x]
+        
+        // aqui onde eh calculado o valor da equacao baseado na string formula
+        if let timesResult = formula.expression.expressionValue(with: intDictionary, context: nil) as? Double {
+            return timesResult
+        } else { return 0 } // caso de algum erro, deve-se ainda fazer uma verificacao mais fiel, por o return 0 causara erros na saida
+    }
+
+    // Mostra o resultado (sucesso ou erro) do cálculo da integral.
+    switch result {
+        case .success(let integralResult):
+            print("Integral calculada:\n\(integralResult)")
+        case .failure(let error):
+            print("Erro no cálculo da integral:", error.errorDescription)
+    }
+    
+    
+    print("""
+
+    -------------------------------------------------------\n
+    Deseja resolver outra integral?
+    ✅  Caso sim, digite "sim" ou  "s"
+    ❌  Caso não, digite qualquer outra tecla
+    """)
+    
+    let inputFinal: String? = readLine()
+    guard inputFinal!.lowercased() == "sim" || inputFinal!.lowercased() == "s" else {
+        break
+    }
+    
+}
+
